130b0b5666d646a84e5a65b4f3cb030f3eea6937
Matt Welsh <mdw@mdw.la>
2022-11-23 20:37:32 -0800

    Update skimapper.py

--- skimapper.py
+++ skimapper.py
@@ -2,13 +2,7 @@
 
 from stravaio import strava_oauth2
 
-CLIENT_ID=97461
-CLIENT_SECRET="1c509fd8f02edac26fd433a0b0d7152eb0fcf3da"
-ACCESS_TOKEN="b89ebb0678b61d4627346b4a53c1ac740130b42b"
-REFRESH_TOKEN="67acbab19db7d0e2643b3722d949c07f1832be6d"
 
-def get_access_token():
-    strava_oauth2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
 
 
